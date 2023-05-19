bb4c3c98cb4fcb0dc176471456f310166f8c72f2
marceloambarc <marcelo.ambarc@gmail.com>
2023-05-19 15:21:56 -0300

    First Commit

---
+++ server/src/server.ts
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+
+import fastify from 'fastify';
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
+
+const app = fastify();
+
+app.register(cors, {
+    origin: true
+})
+
+app.register(jwt, {
+    secret: 'mambarcspacetime'
+})
+
+app.register(authRoutes)
+app.register(memoriesRoutes)
+
+app.listen({
+    port: 3333,
+    host: '0.0.0.0',
+}).then(() => {
+    console.log('Servidor na Porta 3333');
+});
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+export function Copyright() {
+    return (
+        <div className='text-sm leading-relaxed text-gray-200'>
+            Evento da NLW <a href='http://rocketseat.com.br' className='underline hover:text-gray-100'>RocektSeat</a>
+        </div>
+    )
+}
